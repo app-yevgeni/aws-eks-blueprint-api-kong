a28aeaf1cab254209d572a814b07d296f4b1b41d
app-yevgeni <23049337+app-yevgeni@users.noreply.github.com>
2026-07-03 23:39:40 +0300

    update

--- resources.tf
+++ resources.tf
@@ -17,9 +17,14 @@ module "kong" {
   depends_on = [module.httpd]
 }
 
+module "kong-session" {
+  source = "./plugins/kong-session"
+  depends_on = [module.kong]
+}
+
 module "ingress" {
   source = "./modules/ingress"
-  depends_on = [module.kong]
+  depends_on = [module.kong-session]
 }
 
 
