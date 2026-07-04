dfbf913f6880a5a842e85f13ccaef78b5fdb5e20
app-yevgeni <23049337+app-yevgeni@users.noreply.github.com>
2026-07-04 20:47:05 +0300

    Update main.tf

--- modules/kong/main.tf
+++ modules/kong/main.tf
@@ -8,9 +8,6 @@ resource "helm_release" "kong" {
   chart      = "kong"
 
   set = [
-    # ---------------------------
-    # Ingress / Gateway API
-    # ---------------------------
     {
       name  = "ingressController.installCRDs"
       value = "false"
@@ -19,18 +16,6 @@ resource "helm_release" "kong" {
       name  = "ingressController.gatewayAPI.enabled"
       value = "true"
     },
-
-    # ---------------------------
-    # Replicas
-    # ---------------------------
-    {
-      name  = "replicaCount"
-      value = "2"
-    },
-
-    # ---------------------------
-    # Service exposure (NLB)
-    # ---------------------------
     {
       name  = "proxy.type"
       value = "LoadBalancer"
@@ -39,10 +24,6 @@ resource "helm_release" "kong" {
       name  = "proxy.externalTrafficPolicy"
       value = "Cluster"
     },
-
-    # ---------------------------
-    # AWS NLB configuration
-    # ---------------------------
     {
       name  = "proxy.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
       value = "nlb"
@@ -54,50 +35,6 @@ resource "helm_release" "kong" {
     {
       name  = "proxy.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-nlb-target-type"
       value = "ip"
-    },
-    {
-      name  = "proxy.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-attributes"
-      value = "load_balancing.cross_zone.enabled=true"
-    },
-
-    # ---------------------------
-    # Health checks
-    # ---------------------------
-    {
-      name  = "proxy.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-healthcheck-protocol"
-      value = "HTTP"
-    },
-    {
-      name  = "proxy.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-healthcheck-path"
-      value = "/status"
-    },
-
-    # ---------------------------
-    # Stability under rollout / scale
-    # ---------------------------
-    {
-      name  = "proxy.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-target-group-attributes"
-      value = "deregistration_delay.timeout_seconds=30"
-    },
-
-    # ---------------------------
-    # Multi-AZ pod distribution
-    # ---------------------------
-    {
-      name  = "proxy.affinity.podAntiAffinity"
-      value = "soft"
-    },
-    {
-      name  = "proxy.topologySpreadConstraints[0].maxSkew"
-      value = "1"
-    },
-    {
-      name  = "proxy.topologySpreadConstraints[0].topologyKey"
-      value = "topology.kubernetes.io/zone"
-    },
-    {
-      name  = "proxy.topologySpreadConstraints[0].whenUnsatisfiable"
-      value = "ScheduleAnyway"
     }
   ]
 }
